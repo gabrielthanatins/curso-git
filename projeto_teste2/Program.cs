6aeb63a5527bcd87bd8e880c399221c8009e3c1a
Gabriel Evaristo <gabriel-evaristo@hotmail.com>
2021-06-28 17:10:26 -0300

    Acrescentada a pergunta como voce esta hoje

--- projeto_teste2/Program.cs
+++ projeto_teste2/Program.cs
@@ -5,6 +5,7 @@ namespace projeto_teste2 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje? ");
         }
     }
 }
