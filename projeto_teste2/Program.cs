137638a5aeedf6bbe87543980b55fe29e370a4d7
Gabriel Evaristo <gabriel-evaristo@hotmail.com>
2021-06-28 11:29:45 -0300

    Acrescentada uma mensagem de bom dia

--- projeto_teste2/Program.cs
+++ projeto_teste2/Program.cs
@@ -4,6 +4,7 @@ namespace projeto_teste2 {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia");
         }
     }
 }
